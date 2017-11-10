e35968ed3a57b561744b1c1c79c90c41214ed80b
benzsos <t_tofuz_bbbenz@hotmail.com>
2017-11-10 16:15:56 +0700

    start

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require 'include/connect.php';
 $Chk=0;
 $result = mysql_query( "SELECT Chk FROM chart_menu" );
@@ -13,9 +13,9 @@ while ($Row = mysql_fetch_array($result))
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Fai Bakery</title>
+  <title>Fai Bakery</title> 
   <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
-  
+
   <style>
 		.button {
 		  display: inline-block;
@@ -31,14 +31,14 @@ while ($Row = mysql_fetch_array($result))
 		  cursor: pointer;
 		  margin: 5px;
 		}
-		
+
 		.button span {
 		  cursor: pointer;
 		  display: inline-block;
 		  position: relative;
 		  transition: 0.5s;
 		}
-		
+
 		.button span:after {
 		  content: '\00bb';
 		  position: absolute;
@@ -47,16 +47,16 @@ while ($Row = mysql_fetch_array($result))
 		  right: -20px;
 		  transition: 0.5s;
 		}
-		
+
 		.button:hover span {
 		  padding-right: 25px;
 		}
-		
+
 		.button:hover span:after {
 		  opacity: 1;
 		  right: 0;
 		}
-		
+
 		.enjoy-css {
 		  -webkit-box-sizing: content-box;
 		  -moz-box-sizing: content-box;
@@ -74,7 +74,7 @@ while ($Row = mysql_fetch_array($result))
 		  -o-transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
 		  transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
 		}
-		
+
 		.enjoy-css:hover {
 		  color: rgba(173,167,211,1);
 		  text-shadow: 0 1px 0 rgba(255,255,255,1) , 0 2px 0 rgba(255,255,255,1) , 0 3px 0 rgba(255,255,255,1) , 0 4px 0 rgba(255,255,255,1) , 0 5px 0 rgba(255,255,255,1) , 0 6px 1px rgba(0,0,0,0.0980392) , 0 0 5px rgba(0,0,0,0.0980392) , 0 1px 3px rgba(0,0,0,0.298039) , 0 3px 5px rgba(0,0,0,0.2) , 0 -5px 10px rgba(0,0,0,0.247059) , 0 -7px 10px rgba(0,0,0,0.2) , 0 -15px 20px rgba(0,0,0,0.14902) ;
@@ -122,11 +122,11 @@ while ($Row = mysql_fetch_array($result))
 
                 <span style="cursor:pointer">
                 <? if($Chk==1){ ?>
-                	<img src="images/check01.png" id="im02" width="25px" height="25px"> 
+                	<img src="images/check01.png" id="im02" width="25px" height="25px">
 				<? } ?>
                 1. กราฟ ยอดขายประจำเดือน
                 </span>
- 
+
           </button>
       </td>
       <td>
@@ -135,7 +135,7 @@ while ($Row = mysql_fetch_array($result))
       		<span style="cursor:pointer">
             <? if($Chk==5){ ?><img src="images/check01.png" id="im02" width="25px" height="25px"> <? } ?>
             5. กราฟ Growth เปอร์เซนต์การโตยอดขายแต่ละสาขา</span>
-        </button> 
+        </button>
       </td>
     </tr>
     <tr>
@@ -144,15 +144,15 @@ while ($Row = mysql_fetch_array($result))
       		<span style="cursor:pointer">
             <? if($Chk==2){ ?><img src="images/check01.png" id="im02" width="25px" height="25px"> <? } ?>
             2. กราฟ เปรียบเทียบยอดขายรวมทุกสาขา ประจำเดือน</span>
-        </button>    
+        </button>
       </td>
       <td>
-      	
+
           <button class="button" style="vertical-align:middle" onClick="c01(9)">
             <span style="cursor:pointer">
                 <? if($Chk==9){ ?><img src="images/check01.png" id="im02" width="25px" height="25px"> <? } ?>
                 6. กราฟ เปรียบเทียบยอดขาย การตลาด ต่อเดือน</span>
-          </button> 
+          </button>
       </td>
     </tr>
     <tr>
@@ -161,14 +161,14 @@ while ($Row = mysql_fetch_array($result))
       		<span style="cursor:pointer">
             <? if($Chk==3){ ?><img src="images/check01.png" id="im02" width="25px" height="25px"> <? } ?>
             3. กราฟ เปรียบเทียบยอดขายสาขา</span>
-          </button> 
+          </button>
       </td>
       <td>
             <button class="button" style="vertical-align:middle" onClick="c01(7)">
                <span style="cursor:pointer">
                 <? if($Chk==7){ ?><img src="images/check01.png" id="im02" width="25px" height="25px"> <? } ?>
                 7. กราฟ เปรียบเทียบยอดขาย สายส่ง ต่อเดือน</span
-            ></button> 
+            ></button>
 
       </td>
     </tr>
@@ -178,14 +178,14 @@ while ($Row = mysql_fetch_array($result))
       		<span style="cursor:pointer">
             <? if($Chk==4){ ?><img src="images/check01.png" id="im02" width="25px" height="25px"> <? } ?>
             4. กราฟ เปรียบเทียบ % ของเสีย สาขาต่อเดือน</span>
-        </button>  
+        </button>
       </td>
       <td>
         <button class="button" style="vertical-align:middle" onClick="c01(8)">
       		<span style="cursor:pointer">
             <? if($Chk==8){ ?><img src="images/check01.png" id="im02" width="25px" height="25px"> <? } ?>
             8. กราฟ เปรียบเทียบยอดขาย เซเว่น ต่อเดือน</span>
-        </button> 
+        </button>
       </td>
     </tr>
 
